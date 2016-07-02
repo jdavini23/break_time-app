0932adc879d981f68dc6719ab97662a20d286181
Joe Davini <joedavini23@yahoo.com>
2016-07-02 14:50:16 -0700

    Add files via upload

--- break_time.py
+++ break_time.py
@@ -6,6 +6,6 @@ break_count = 0
 
 print("This program started on "+time.ctime())
 while(break_count < total_breaks):
-    time.sleep(10)
-    webbrowser.open("https://www.youtube.com/watch?v=6-a7t7rJycQ")
+    time.sleep(10) # Change this to the time you want to take breaks at. In seconds
+    webbrowser.open("https://www.youtube.com/watch?v=6-a7t7rJycQ") # You can change this URL to any song
     break_count = break_count + 1
